c7b665efdacfdaaeb2d51d5166ed0d1b80f49df9
Mikko <Andriamanitra@users.noreply.github.com>
2016-04-04 18:12:44 +0300

    Update shitfill2.js

--- shitfill2.js
+++ shitfill2.js
@@ -21,8 +21,7 @@ function fill (xx, yy, cc, aa) {
 
   var pixeldata1 = gpd(xx, yy);
   rgbcc = hexToRgb(cc);
-  // blendataan jotta värivalitsin ottaa oikean värin myös pikseleissä
-  // joissa alpha < 255; 255 255 255 koska taustaväri on valkoinen.
+  // blendataan fill taustaväriin
   pixeldata1.data[0] = rgbcc.r*aa + pixeldata0.data[0]*(1.0-aa);
   pixeldata1.data[1] = rgbcc.g*aa + pixeldata0.data[1]*(1.0-aa);
   pixeldata1.data[2] = rgbcc.b*aa + pixeldata0.data[2]*(1.0-aa);
