94749c641a691a52893ff12f63b9737aa8ce507e
Andriamanitra <mikko77792@gmail.com>
2016-04-11 21:02:17 +0300

    lisää piirtorajaa

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/draw.html');
 });
 var anoncount = 0;
-var muodot_max_length = 500;
+var muodot_max_length = 1200;
 var vuoron_pituus = 120000; // millisekunteina
 var muodot = [];
 var userlist = [];
